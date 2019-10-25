0a335c207797b545061f7a90fb38d1302b915bea
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-10-25 14:11:40 +0700

    Images

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,19 @@
 <script>
 	export let name;
+	let world = "Tinnakorn";
+	let src   = 'assets/image/svelte.png';
 </script>
 
 <style>
 	h1 {
 		color: purple;
+		font-family: 'Comic Sans MS';
+		font-size: 2em;
 	}
 </style>
 
 <h1>Hello {name}!</h1>
+<br>
+<h1>Hello {world.toUpperCase()}!</h1>
+<br>
+<img {src} alt="{name} dances.">
