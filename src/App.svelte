ddebd5540368392b6f42d0405ffaa9f95e9ba321
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-10-25 14:19:44 +0700

    Html And Button

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,38 @@
 <script>
+	import Nested from './Nested.svelte';
 	export let name;
-	let world = "Tinnakorn";
-	let src   = 'assets/image/svelte.png';
+	let world  = "Tinnakorn";
+	let src    = 'assets/image/svelte.png';
+	let string = `this string contains some <strong>HTML!!!</strong>`;
+
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
 </script>
 
 <style>
 	h1 {
 		color: purple;
-		font-family: 'Comic Sans MS';
-		font-size: 2em;
 	}
 </style>
 
 <h1>Hello {name}!</h1>
+
 <br>
+
 <h1>Hello {world.toUpperCase()}!</h1>
+
 <br>
-<img {src} alt="{name} dances.">
+
+<img {src} alt="{name} dances." width="10%">
+
+<!-- import component -->
+<Nested/>
+
+<p>{@html string}</p>
+
+<button on:click={handleClick}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
